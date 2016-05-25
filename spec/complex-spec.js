6060128aa709b2c161bbd9ef6bb90e700dbb6173
kigold <kinglseybox@yahoo.com>
2016-05-25 16:24:56 +0100

    add test cases

--- spec/complex-spec.js
+++ spec/complex-spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//var Complex = require("../complex");
+var Complex = require("../complex");
 
 describe('After Initialisation of Complex Number', function (){
 	var complexNumber
@@ -8,19 +8,75 @@ describe('After Initialisation of Complex Number', function (){
 	    complexNumber = new Complex(3, 2);
 	});
 
-		it('it should Have a real number 3', function() {
+		it('should Have a real number 3', function() {
 		    expect(complexNumber.real).toEqual(3);
 	  	});
 
-	  	it('it should Have an imaginary number 2', function() {
+	  	it('should Have an imaginary number 2', function() {
 		    expect(complexNumber.imag).toEqual(2);
 	  	});
 
-	  	it('Adding 3+2i to 3+2i the imaginary number supposed to be ', function() {
+	  	it('should Add complex number ', function() {
 		    expect(complexNumber.add(complexNumber).imag).toEqual(4);
+		    expect(complexNumber.real).toEqual(6);
 	  	})
 
-	  	it('Adding 3+2i to 3+2i the real number supposed to be ', function() {
-		    expect(complexNumber.add(complexNumber).real).toEqual(6);
+	  	it('should Add real number ', function() {
+		    expect(complexNumber.add(2).imag).toEqual(2);
+		    expect(complexNumber.real).toEqual(5);
 	  	})
+
+	  	it('should Subtract complex number ', function() {
+		    expect(complexNumber.subtract(new Complex(2,1)).imag).toEqual(1);
+		    expect(complexNumber.real).toEqual(1);
+	  	})
+
+	  	it('should Subtract complex number ', function() {
+		    expect(complexNumber.subtract(new Complex(3,2)).imag).toEqual(0);
+		    expect(complexNumber.real).toEqual(0);
+	  	})
+
+	  	it('should Subtract complex number ', function() {
+		    expect(complexNumber.subtract(new Complex(5,3)).imag).toEqual(-1);
+		    expect(complexNumber.real).toEqual(-2);
+	  	})
+
+	  	it('should Subtract real number ', function() {
+		    expect(complexNumber.subtract(6).imag).toEqual(2);
+		    expect(complexNumber.real).toEqual(-3);
+	  	})
+
+	  	it('should Multiply complex number ', function() {
+		    expect(complexNumber.multiply(new Complex(2,1)).imag).toEqual(7);
+		    expect(complexNumber.real).toEqual(4);
+	  	})
+
+	  	it('should Multiply complex number ', function() {
+		    expect(complexNumber.multiply(new Complex(1,4)).imag).toEqual(14);
+		    expect(complexNumber.real).toEqual(-5);
+	  	})
+
+	  	it('should Multiply real number ', function() {
+		    expect(complexNumber.multiply(6).imag).toEqual(12);
+		    expect(complexNumber.real).toEqual(18);
+	  	})
+
+	  	it('should Multiply negative real number ', function() {
+		    expect(complexNumber.multiply(-4).imag).toEqual(-8);
+		    expect(complexNumber.real).toEqual(-12);
+	  	})
+
+	  	it('should be divided by complex number ', function() {
+	  		complexNumber = new Complex(2,8)
+		    expect(complexNumber.divide(new Complex(1,2)).imag).toEqual(0.8);
+		    expect(complexNumber.real).toEqual(3.6);
+	  	})
+
+	  	it('should be divided by real number ', function() {
+		    expect(complexNumber.divide(2).imag).toEqual(1);
+		    expect(complexNumber.real).toEqual(1.5);
+	  	})
+
 });
+
+///Calculate
