b3fc768fe4f1cd88b64373a43c3f7d11b76a2720
kigold <kinglseybox@yahoo.com>
2016-05-25 14:36:58 +0100

    setup Jasmine test suit

--- complex.js
+++ complex.js
@@ -1,6 +1,9 @@
-exports.Complex = function (real, imag) {
-	//this.real = real;
-	//this.imag = imag;
+var Complex = function (real, imag) {
+//var Complex = function (real, imag) {
+	this.real = real;
+	this.imag = imag;
+
 	return real + imag
 
 }
+module.exports = Complex;
--- spec/complex-spec.js
+++ spec/complex-spec.js
@@ -1,8 +1,25 @@
+'use strict';
+
 var Complex = require("../complex");
 
 describe("Complex", function () {
   it("should add 4 + 5", function () {
-    var number = Complex.Complex(4, 5);
+    var number = Complex(4, 5);
     expect(number).toBe(9);
   });
+});
+
+describe('After Initialisation of Complex Number', function (){
+	var complexNumber
+	beforeEach(function() {
+	    complexNumber = new Complex(3, 2);
+	});
+
+		it('it should Have a real number 3', function() {
+		    expect(complexNumber.real).toEqual(3);
+	  	});
+
+	  	it('it should Have an imaginary number 2', function() {
+		    expect(complexNumber.imag).toEqual(2);
+	  	});
 });
